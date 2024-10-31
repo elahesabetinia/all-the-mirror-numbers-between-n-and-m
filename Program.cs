5f4ca35c8e481054e9d4015ac9c965ce0fe36cb6
elahe sabetinia <saikikusou35@gmail.com>
2024-10-31 10:11:21 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nMirror numbers between {n} and {m} are:");
+
+        for (int i = n; i <= m; i++)
+            if (IsMirrorNumber(i))
+                Console.Write(i + " ");
+
+        Console.WriteLine();
+    }
+
+    static bool IsMirrorNumber(int num)
+    {
+        int original = num, reversed = 0;
+
+        while (num > 0)
+        {
+            int remainder = num % 10;
+            reversed = reversed * 10 + remainder;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
+}
